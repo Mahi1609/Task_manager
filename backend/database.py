5e33f4dbaf0bf0a409cccc06573aff6f8b7ace49
Mahesh <brightapproachsolutions@gmail.com>
2026-06-24 22:27:13 +0530

    configured for postgre

--- backend/database.py
+++ backend/database.py
@@ -1,19 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./tasks.db"
+load_dotenv()
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}  # Needed for SQLite
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
 )
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
 
-# Dependency for DB session
 def get_db():
     db = SessionLocal()
     try:
